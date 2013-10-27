948d81597ce2ae7dcb688af23197da91be32d0fd
Pablo Recio <pablo@potatolondon.com>
2013-10-27 12:01:20 +0000

    Filtering by date range

--- tube/views.py
+++ tube/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import generics
 
 from .models import TubeDisruption
@@ -9,3 +11,18 @@ class DisruptionsView(generics.ListAPIView):
     serializer_class = TubeDisruptionSerializer
     paginate_by = 100
     filter_fields = ('line', 'description', 'start', 'end')
+
+    def get_queryset(self):
+        start = self.request.QUERY_PARAMS.get('start__gt')
+        end = self.request.QUERY_PARAMS.get('end__lt')
+
+        qs = super(DisruptionsView, self).get_queryset()
+        filters = {}
+        if start:
+            filters['start__gt'] = datetime.strptime(start, "%Y-%m-%d")
+        if end:
+            filters['end__lt'] = datetime.strptime(end, "%Y-%m-%d")
+
+        if filters:
+            qs = qs.filter(**filters)
+        return qs
